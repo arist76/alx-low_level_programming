ada606d5a2886d7bdc1f3f9e2d7ac93f89c04f30
arist76 <surafelfikru76@gmail.com>
2022-06-03 16:04:46 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - prints the last digit
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int remainder = n % 10;
+
+	if (remainder > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, remainder);
+	else if (remainder == 0)
+		printf("Last digit of %d is %d and is 0", n, remainder);
+	else if (remainder < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, remainder);
+	return (0);
+}

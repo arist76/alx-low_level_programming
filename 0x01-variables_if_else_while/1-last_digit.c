16681cb90e3a0bdb615957cd4cf08d1ec39937ea
arist76 <surafelfikru76@gmail.com>
2022-06-03 16:25:13 -0700

    fix betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,6 +25,7 @@ int main(void)
 	else if (remainder == 0)
 		printf("Last digit of %d is %d and is 0\n", n, remainder);
 	else if (remainder < 6)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, remainder);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+n, remainder);
 	return (0);
 }

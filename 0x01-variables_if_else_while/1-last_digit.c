a3bde43f6a64b8300bb0b02d0608627dc518bf61
arist76 <surafelfikru76@gmail.com>
2022-06-03 16:17:26 -0700

    fix betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,11 +12,12 @@
 int main(void)
 {
 	int n;
+	int remainder;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int remainder; 
+
 	remainder = n % 10;
 
 	if (remainder > 5)

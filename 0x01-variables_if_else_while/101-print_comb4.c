ae5388ac037961a14ac4598e1535d7c6b75f3b6a
arist76 <surafelfikru76@gmail.com>
2022-06-05 17:45:06 +0300

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination of two numbers
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	/* 0 - 48 and 9 - 57*/
+
+	int end = 57;
+	int start1;
+	int start2;
+	int start3;
+
+	for (start1 = 48; start1 <= end; start1++)
+	{
+		for (start2 = start1 + 1; start2 <= end; start2++)
+		{
+			for (start3 = start2 + 1; start3 <= end; start3++)
+			{
+				putchar(start1);
+				putchar(start2);
+				putchar(start3);
+				if (start1 == 58 && start2 == 56 && start3 == 57)
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}

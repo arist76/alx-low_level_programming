8cd363c122a2b402ba967d2f662baf128c2fb29a
arist76 <surafelfikru76@gmail.com>
2022-06-08 18:39:51 +0300

    fix header error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
-#include <_putchar.c>
+#include "main.h"
 
 int main(void)
 {
-	char x[7] = {95, 112, 117, 116, 99, 104, 97, 114};
+	char x[8] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int i, j;
 
-	for (i = 0; i <= 7; i++:)
+	for (i = 0; i <= 7; i++)
 	{
 		_putchar(x[i]);
 	}
 
 	_putchar('\n');
-	return (0);`
+	return (0);
 }

738a78cf68cb983e7adc572e561ea4160406ea6f
arist76 <surafelfikru76@gmail.com>
2022-06-08 18:16:39 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <_putchar.c>
+
+int main(void)
+{
+	char x[7] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i, j;
+
+	for (i = 0; i <= 7; i++:)
+	{
+		_putchar(x[i]);
+	}
+
+	_putchar('\n');
+	return (0);`
+}

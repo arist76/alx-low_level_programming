59c82f6a17ad8140e52f177590a8c1e76457d796
arist76 <surafelfikru76@gmail.com>
2022-06-22 21:23:20 +0300

    fix bugs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /*
  * print_sign - checks for integer sign
  * @n: the number to be checked
- * 
+ *
  * Return: returns 1,0 or -1
  */
 
@@ -17,7 +17,7 @@ int print_sign(int n)
 	{
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		return (-1);
 	}

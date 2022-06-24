dc20c21e01fc53d3c0ffe1164a51a6979323ecc5
arist76 <surafelfikru76@gmail.com>
2022-06-24 17:48:07 +0300

    fix unused argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
  * main - prints the name of the executable
  * @argc: number of CLAs
  * @argv: array of stings of the CLAs
- * Return: returns 1
+ * Return: returns argc
  */
 
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (1);
+	return (argc);
 }

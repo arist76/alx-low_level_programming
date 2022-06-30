79d62258aa56880718f7ede782466eff21123983
arist76 <surafelfikru76@gmail.com>
2022-06-30 16:44:19 +0300

    first two tasks

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates array of size with value c
+ * @size: the size of the array
+ * @c: the initalization character of the array
+ * Return: returns the pointer to the first value
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ars;
+	int i;
+
+	ars = (char *) malloc(sizeof(char) * size);
+
+	if (size == 0 || ars == NULL)
+	{
+		return (NULL);
+	}
+	;
+
+	for (i = 0; i <= size; i++)
+	{
+		ars[i] = c;
+	}
+
+	return (&ars[0]);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * _strdup - copies a string
+ * @str: the string to be copied
+ * Return: the string to be copied
+ */
+char *_strdup(char *str)
+{
+	char *cpy;
+	int size, i;
+
+	/* find the size of str */
+	size = 0;
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+
+	/* allocate memory */
+	cpy = (char *) malloc(size);
+	if (cpy == NULL || size == 0)
+	{
+		return (NULL);
+	}
+
+
+	/* copy str */
+	for (i = 0; i <= size; i++)
+	{
+		*(cpy + i) = str[i];
+	}
+
+	return (cpy);
+}

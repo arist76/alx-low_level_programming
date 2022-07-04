38ecfd6014f89de1fe9dc512f8b7e679fb5ee51b
arist76 <surafelfikru76@gmail.com>
2022-07-05 02:02:37 +0300

    fix betty

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-
+/**
+ * init_dog - creates a dog
+ * @d: the structure of the dog
+ * @name: the dogs name
+ * @age: the dogs age
+ * @owner: the dogs owner
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }

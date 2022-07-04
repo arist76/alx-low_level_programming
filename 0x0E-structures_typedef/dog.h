4f183c421b2769279ca7d8be36d9404025094cc8
arist76 <surafelfikru76@gmail.com>
2022-07-05 01:38:11 +0300

    fix betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * struct dog - represents a dog
+ * @name: the name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: represents a dog with
+ * a name, age and an owner
+ */
+
 struct dog
 {
 	char *name;

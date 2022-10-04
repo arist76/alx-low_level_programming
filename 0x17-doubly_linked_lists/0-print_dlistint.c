df0e1bebee20776bb793a0eea3c81525f745e89b
arist76 <https://github.com/arist76>
2022-10-04 22:46:46 +0300

    fix up doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,26 +1,22 @@
-#include <stdio.h>
 #include "lists.h"
-
 /**
- * print_dlistint - prints values of a doubly linked list
- * @h: pointer to the head of the list
- * Return: the number of values
+ * print_dlistint - print value of all nodes
+ * @h: pointer to start list
+ * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
-	dlistint_t *temp;
+	size_t con = 0;
 
 	if (h == NULL)
 		return (0);
 
-	temp = h;
-
-	for (count = 0; temp != NULL; count++)
+	while (h)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		con++;
+		h = h->next;
 	}
 
-	return (count);
+	return (con);
 }
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,15 +1,19 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - Return the number of elements in a dlistint_t list
+ * @h: pointer to The list
+ * Return: The number of nodes
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count;
-	dlistint_t *temp;
-	temp = h;
+	size_t con = 0;
 
-	for (count = 0; temp != NULL; count++)
+	while (h)
 	{
-		temp = temp->next;
+		con++;
+		h = h->next;
 	}
 
-	return count;
+	return (con);
 }
